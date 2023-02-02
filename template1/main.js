5e00f8f7d21da0b5a4894810aa743be6edfdfd7e
Gustavo Ferreira <gfmb94@gmail.com>
2023-02-02 16:42:07 -0300

    Ajustando local img

--- template1/main.js
+++ template1/main.js
@@ -27,7 +27,7 @@ document.querySelector('#app').innerHTML = `
 
 <!-- Header -->
 <header class="w3-display-container w3-content w3-wide" style="max-width:1500px;" id="home">
-  <img class="w3-image" src="/assets/first.jpg" alt="mscalhas" width="1500" height="auto">
+  <img class="w3-image" src="/assets/img/first.jpg" alt="mscalhas" width="1500" height="auto">
   <div class="w3-display-middle w3-margin-top w3-center">
     <h1 class="w3-xxlarge w3-text-white"><span class="w3-padding w3-black w3-opacity-min"><b>BR</b></span> <span class="w3-hide-small w3-text-light-grey">Architects</span></h1>
   </div>
