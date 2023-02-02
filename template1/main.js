1486fc861e844528eb9d30bd751055730014f2f0
Gustavo Ferreira <gfmb94@gmail.com>
2023-02-02 16:40:47 -0300

    Ajustando layout toolbar

--- template1/main.js
+++ template1/main.js
@@ -4,29 +4,30 @@ document.querySelector('#app').innerHTML = `
 <!DOCTYPE html>
 <html>
 <head>
-<title>W3.CSS Template</title>
+<title>MS Calhas</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="icon" href="logo.png" type="image/gif" sizes="16x16">
 </head>
 <body>
 
 <!-- Navbar (sit on top) -->
 <div class="w3-top">
   <div class="w3-bar w3-white w3-wide w3-padding w3-card">
-    <a href="#home" class="w3-bar-item w3-button"><b>BR</b> Architects</a>
+    <a href="#home" class="w3-bar-item w3-button"><b>MS</b> Calhas</a>
     <!-- Float links to the right. Hide them on small screens -->
     <div class="w3-right w3-hide-small">
-      <a href="#projects" class="w3-bar-item w3-button">Projects</a>
-      <a href="#about" class="w3-bar-item w3-button">About</a>
-      <a href="#contact" class="w3-bar-item w3-button">Contact</a>
+      <a href="#projects" class="w3-bar-item w3-button">Projetos</a>
+      <a href="#about" class="w3-bar-item w3-button">Sobre</a>
+      <a href="#contact" class="w3-bar-item w3-button">Contato</a>
     </div>
   </div>
 </div>
 
 <!-- Header -->
 <header class="w3-display-container w3-content w3-wide" style="max-width:1500px;" id="home">
-  <img class="w3-image" src="/w3images/architect.jpg" alt="Architecture" width="1500" height="800">
+  <img class="w3-image" src="/assets/first.jpg" alt="mscalhas" width="1500" height="auto">
   <div class="w3-display-middle w3-margin-top w3-center">
     <h1 class="w3-xxlarge w3-text-white"><span class="w3-padding w3-black w3-opacity-min"><b>BR</b></span> <span class="w3-hide-small w3-text-light-grey">Architects</span></h1>
   </div>
