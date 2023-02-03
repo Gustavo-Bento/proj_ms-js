869d323189d47905abac3bcd5db4d94c4ca59d6b
Gustavo Ferreira <gfmb94@gmail.com>
2023-02-02 21:09:31 -0300

    Adicionando comentarios

--- template1/src/App.tsx
+++ template1/src/App.tsx
@@ -10,6 +10,10 @@ import Img7 from './assets/img7.jpg'
 import Img8 from './assets/img8.jpg'
 import Fundo from './assets/fundo.jpg'
 import Logo from './assets/logo.png'
+import Perfil1 from './assets/perfil1.jpg'
+import Perfil2 from './assets/perfil2.jpg'
+import Perfil3 from './assets/perfil3.jpg'
+import Perfil4 from './assets/perfil4.jpg'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -19,7 +23,7 @@ function App() {
       <div className="w3-top">
         <div className="w3-bar w3-white w3-wide w3-padding w3-card">
           <a href="#home" className="w3-bar-item w3-button">
-            <img src={Logo} alt="Logo" style={{ width: '5%' }} className="w3-round" />
+            <a href="#home" class="w3-bar-item w3-button"><b>MS</b> Calhas & Hidraulica</a>
           </a>
           {/* Float links to the right. Hide them on small screens */}
           <div className="w3-right w3-hide-small">
@@ -34,12 +38,12 @@ function App() {
         <img className="w3-image" src={Fundo} alt="mscalhas" width={1500} height="auto" />
         <div className="w3-display-middle w3-margin-top w3-center">
           <img src={Logo} alt="Logo" style={{ width: '20%' }} className="w3-round w3-opacity-min" />
-            <h1 className="w3-xxlarge w3-text-white">
-              <span className="w3-padding w3-opacity-min">
-                <b>MS</b>
-              </span>
-              <span className="w3-hide-small w3-text-light-grey">Calhas</span>
-            </h1>
+          <h1 className="w3-xxlarge w3-text-white">
+            <span className="w3-padding w3-opacity-min">
+              <b>MS</b>
+            </span>
+            <span className="w3-hide-small w3-text-light-grey">Calhas & Hidraulica</span>
+          </h1>
         </div>
       </header>
       {/* Page content */}
@@ -102,37 +106,37 @@ function App() {
         </div>
         {/* About Section */}
         <div className="w3-container w3-padding-32" id="about">
-          <h3 className="w3-border-bottom w3-border-light-grey w3-padding-16">About</h3>
-          <p>Trabalhamos com Calhas, Rufos Pingadeiras, Hidráulica e manutenção. Profissionais qualificados e de confiança.
-          </p>
+          <h3 className="w3-border-bottom w3-border-light-grey w3-padding-16">Sobre a importancia das Calhas</h3>
+          <p>As calhas são uma parte importante de qualquer estrutura de cobertura, pois são essenciais para gerenciar o escoamento de água e evitar danos à fundação e às paredes do edifício. Calhas de metal, como alumínio e aço galvanizado, são as mais comumente usadas, pois são duráveis, resistentes às intempéries e acessíveis. Essas calhas podem ter uma variedade de formas e tamanhos e podem ser instaladas de diferentes maneiras, incluindo cabides tradicionais, cabides ocultos e suportes de fáscia. Para garantir um telhado duradouro e evitar possíveis danos, é importante garantir que as calhas sejam instaladas e mantidas corretamente.</p>
         </div>
         <div className="w3-row-padding w3-grayscale">
           <div className="w3-col l3 m6 w3-margin-bottom">
-            <img src="/w3images/team2.jpg" alt="John" style={{ width: '100%' }} />
-            <h3>John Doe</h3>
-            <p className="w3-opacity">CEO &amp; Founder</p>
-            <p>Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.</p>
+            <img src={Perfil1} alt="John" style={{ width: '100%' }} />
+            <h3>Gustavo Ferreira</h3>
+            <p className="w3-opacity">Professor</p>
+            <p>
+Obrigado por manter diligentemente os azulejos e encanamentos da propriedade.</p>
             <p><button className="w3-button w3-light-grey w3-block">Contact</button></p>
           </div>
           <div className="w3-col l3 m6 w3-margin-bottom">
-            <img src="/w3images/team1.jpg" alt="Jane" style={{ width: '100%' }} />
-            <h3>Jane Doe</h3>
-            <p className="w3-opacity">Architect</p>
-            <p>Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.</p>
+            <img src={Perfil2} alt="Jane" style={{ width: '100%' }} />
+            <h3>Lucas Henrique</h3>
+            <p className="w3-opacity">Estagiário</p>
+            <p>Sua atenção aos detalhes é muito apreciada.</p>
             <p><button className="w3-button w3-light-grey w3-block">Contact</button></p>
           </div>
           <div className="w3-col l3 m6 w3-margin-bottom">
-            <img src="/w3images/team3.jpg" alt="Mike" style={{ width: '100%' }} />
-            <h3>Mike Ross</h3>
-            <p className="w3-opacity">Architect</p>
-            <p>Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.</p>
+            <img src={Perfil3} alt="Guilherme" style={{ width: '100%' }} />
+            <h3>Guilherme Silva</h3>
+            <p className="w3-opacity">CEO da MS</p>
+            <p>O trabalho árduo garante que a propriedade seja mantida em excelentes condições.</p>
             <p><button className="w3-button w3-light-grey w3-block">Contact</button></p>
           </div>
           <div className="w3-col l3 m6 w3-margin-bottom">
-            <img src="/w3images/team4.jpg" alt="Dan" style={{ width: '100%' }} />
-            <h3>Dan Star</h3>
-            <p className="w3-opacity">Architect</p>
-            <p>Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.</p>
+            <img src={Perfil4} alt="Michel" style={{ width: '100%' }} />
+            <h3>Bianca Pires</h3>
+            <p className="w3-opacity">Pedagoga</p>
+            <p>Estamos profundamente gratos por toda a manutenção que você fez.</p>
             <p><button className="w3-button w3-light-grey w3-block">Contact</button></p>
           </div>
         </div>
