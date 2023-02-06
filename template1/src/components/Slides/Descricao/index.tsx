b350fd5f5bb3caab1d7b280e499fd3ba101ee25e
Gustavo Ferreira <gfmb94@gmail.com>
2023-02-06 15:41:34 -0300

    Corrigindo classNames

--- template1/src/components/Slides/Descricao/index.tsx
+++ template1/src/components/Slides/Descricao/index.tsx
@@ -2,18 +2,18 @@ function Descricao() {
     return (
         <div className="w3-content w3-padding">
             
-            <div class="w3-card-4" style="width:50%;">
-                <header class="w3-container w3-light-grey">
+            <div className="w3-card-4" style="width:50%;">
+                <header className="w3-container w3-light-grey">
                 <h2>Soluções para Calhas e Hidraulica</h2>
                 </header>
 
-                <div class="w3-container">
+                <div className="w3-container">
                     <p style="text-align:justify">
                         As calhas são uma parte importante de qualquer estrutura de cobertura, pois são essenciais para gerenciar o escoamento de água e evitar danos à fundação e às paredes do edifício. Calhas de metal, como alumínio e aço galvanizado, são as mais comumente usadas, pois são duráveis, resistentes às intempéries e acessíveis. Essas calhas podem ter uma variedade de formas e tamanhos e podem ser instaladas de diferentes maneiras, incluindo cabides tradicionais, cabides ocultos e suportes de fáscia. Para garantir um telhado duradouro e evitar possíveis danos, é importante garantir que as calhas sejam instaladas e mantidas corretamente.
                     </p>
                 </div>
 
-                <footer class="w3-container w3-light-grey">
+                <footer className="w3-container w3-light-grey">
                     <h5>Guilherme Silva (CEO)r</h5>
                 </footer>
             </div>
