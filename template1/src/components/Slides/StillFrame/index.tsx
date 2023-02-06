9b23c3b8e20956fb66ad75c095a00247596e7a76
Gustavo Ferreira <gfmb94@gmail.com>
2023-02-06 14:37:14 -0300

    Implementando carrossel com bootstrap

--- template1/src/components/Slides/StillFrame/index.tsx
+++ template1/src/components/Slides/StillFrame/index.tsx
@@ -1,45 +1,49 @@
-import '../../../App.css'
-import './style.css'
-import './showDiv'
 
 import Img1 from '../../../assets/img_forest.jpg'
 import Img2 from '../../../assets/img_lights.jpg'
 import Img3 from '../../../assets/img_mountains.jpg'
 import Img4 from '../../../assets/img_showtops.jpg'
 
-function showDivs(n) {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    if (n > x.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = x.length }
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-    x[slideIndex - 1].style.display = "block"
-}
-
-function plusDivs(n) {
-    showDivs(slideIndex += n);
-}
-
-
 function StillFrame() {
-    var slideIndex = 1;
-    showDivs(slideIndex);
 
     return (
-        <div className="w3-col l3 m6 w3-margin-bottom">
-            <div className="w3-display-container">
-                <div className="w3-display-topleft w3-indigo w3-padding w3-opacity-min">Estrutura</div>
-                <div className="w3-content w3-display-container">
-                    <img className="mySlides" src={Img1} style="width:100%" />
-                    <img className="mySlides" src={Img2} style="width:100%" />
-                    <img className="mySlides" src={Img3} style="width:100%" />
-                    <img className="mySlides" src={Img4} style="width:100%" />
+        <div className="container">
+            <h2>Carousel Example</h2>
+            <div id="myCarousel" className="carousel slide" data-ride="carousel">
+                <!-- Indicators -->
+                <ol className="carousel-indicators">
+                    <li data-target="#myCarousel" data-slide-to="0" className="active"></li>
+                    <li data-target="#myCarousel" data-slide-to="1"></li>
+                    <li data-target="#myCarousel" data-slide-to="2"></li>
+                    <li data-target="#myCarousel" data-slide-to="3"></li>
+                </ol>
 
-                    <button className="w3-button w3-black w3-display-left" onclick="plusDivs(-1)">&#10094;</button>
-                    <button className="w3-button w3-black w3-display-right" onclick="plusDivs(1)">&#10095;</button>
+                <!-- Wrapper for slides -->
+                <div className="carousel-inner">
+                    <div className="item active">
+                        <img src={Img1} alt="Forest" style="width:100%;">
+                    </div>
+                    <div className="item">
+                        <img src={Img2} alt="Lights" style="width:100%;">
+                    </div>
+                    <div className="item">
+                        <img src={Img3} alt="Mountains" style="width:100%;">
+                    </div>
+
+                    <div className="item">
+                    <img src={Img4} alt="Showtops" style="width:100%;">
+                    </div>
                 </div>
+
+                <!-- Left and right controls -->
+                <a className="left carousel-control" href="#myCarousel" data-slide="prev">
+                    <span className="glyphicon glyphicon-chevron-left"></span>
+                    <span className="sr-only">Previous</span>
+                </a>
+                <a className="right carousel-control" href="#myCarousel" data-slide="next">
+                    <span className="glyphicon glyphicon-chevron-right"></span>
+                    <span className="sr-only">Next</span>
+                </a>
             </div>
         </div>
     )
