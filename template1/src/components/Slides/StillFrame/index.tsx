d15e4d09be3f9a6b6d4f37088e265b0447f0c6a8
Gustavo Ferreira <gfmb94@gmail.com>
2023-02-06 14:05:51 -0300

    Corrigindo classnames

--- template1/src/components/Slides/StillFrame/index.tsx
+++ template1/src/components/Slides/StillFrame/index.tsx
@@ -29,14 +29,14 @@ function StillFrame() {
         <div className="w3-col l3 m6 w3-margin-bottom">
             <div className="w3-display-container">
                 <div className="w3-display-topleft w3-indigo w3-padding w3-opacity-min">Estrutura</div>
-                <div clasName="w3-content w3-display-container">
-                    <img clasName="mySlides" src={Img1} style="width:100%" />
-                    <img clasName="mySlides" src={Img2} style="width:100%" />
-                    <img clasName="mySlides" src={Img3} style="width:100%" />
-                    <img clasName="mySlides" src={Img4} style="width:100%" />
+                <div className="w3-content w3-display-container">
+                    <img className="mySlides" src={Img1} style="width:100%" />
+                    <img className="mySlides" src={Img2} style="width:100%" />
+                    <img className="mySlides" src={Img3} style="width:100%" />
+                    <img className="mySlides" src={Img4} style="width:100%" />
 
-                    <button clasName="w3-button w3-black w3-display-left" onclick="plusDivs(-1)">&#10094;</button>
-                    <button clasName="w3-button w3-black w3-display-right" onclick="plusDivs(1)">&#10095;</button>
+                    <button className="w3-button w3-black w3-display-left" onclick="plusDivs(-1)">&#10094;</button>
+                    <button className="w3-button w3-black w3-display-right" onclick="plusDivs(1)">&#10095;</button>
                 </div>
             </div>
         </div>
