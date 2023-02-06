99e213132c27d9219e64f9bce601352b2b8f78f4
Gustavo Ferreira <gfmb94@gmail.com>
2023-02-06 14:40:32 -0300

    Convertendo html to tsx

--- template1/src/components/Slides/StillFrame/index.tsx
+++ template1/src/components/Slides/StillFrame/index.tsx
@@ -5,47 +5,40 @@ import Img3 from '../../../assets/img_mountains.jpg'
 import Img4 from '../../../assets/img_showtops.jpg'
 
 function StillFrame() {
-
     return (
-        <div className="container">
-            <h2>Carousel Example</h2>
-            <div id="myCarousel" className="carousel slide" data-ride="carousel">
-                <!-- Indicators -->
-                <ol className="carousel-indicators">
-                    <li data-target="#myCarousel" data-slide-to="0" className="active"></li>
-                    <li data-target="#myCarousel" data-slide-to="1"></li>
-                    <li data-target="#myCarousel" data-slide-to="2"></li>
-                    <li data-target="#myCarousel" data-slide-to="3"></li>
-                </ol>
-
-                <!-- Wrapper for slides -->
-                <div className="carousel-inner">
-                    <div className="item active">
-                        <img src={Img1} alt="Forest" style="width:100%;">
-                    </div>
-                    <div className="item">
-                        <img src={Img2} alt="Lights" style="width:100%;">
-                    </div>
-                    <div className="item">
-                        <img src={Img3} alt="Mountains" style="width:100%;">
-                    </div>
-
-                    <div className="item">
-                    <img src={Img4} alt="Showtops" style="width:100%;">
-                    </div>
-                </div>
-
-                <!-- Left and right controls -->
-                <a className="left carousel-control" href="#myCarousel" data-slide="prev">
-                    <span className="glyphicon glyphicon-chevron-left"></span>
-                    <span className="sr-only">Previous</span>
-                </a>
-                <a className="right carousel-control" href="#myCarousel" data-slide="next">
-                    <span className="glyphicon glyphicon-chevron-right"></span>
-                    <span className="sr-only">Next</span>
-                </a>
+        <div classname="container">
+        <h2>Carousel Example</h2>
+        <div id="myCarousel" classname="carousel slide" data-ride="carousel">
+          <ol classname="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to={0} classname="active" />
+            <li data-target="#myCarousel" data-slide-to={1} />
+            <li data-target="#myCarousel" data-slide-to={2} />
+            <li data-target="#myCarousel" data-slide-to={3} />
+          </ol>
+          <div classname="carousel-inner">
+            <div classname="item active">
+              <img src="{Img1}" alt="Forest" style={{width: '100%'}} />
             </div>
+            <div classname="item">
+              <img src="{Img2}" alt="Lights" style={{width: '100%'}} />
+            </div>
+            <div classname="item">
+              <img src="{Img3}" alt="Mountains" style={{width: '100%'}} />
+            </div>
+            <div classname="item">
+              <img src="{Img4}" alt="Showtops" style={{width: '100%'}} />
+            </div>
+          </div>
+          <a classname="left carousel-control" href="#myCarousel" data-slide="prev">
+            <span classname="glyphicon glyphicon-chevron-left" />
+            <span classname="sr-only">Previous</span>
+          </a>
+          <a classname="right carousel-control" href="#myCarousel" data-slide="next">
+            <span classname="glyphicon glyphicon-chevron-right" />
+            <span classname="sr-only">Next</span>
+          </a>
         </div>
+      </div>
     )
 }
 
