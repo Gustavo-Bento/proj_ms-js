cf7297ee53c17a6bf24dc3c06a022107d77b0c70
Gustavo Ferreira <gfmb94@gmail.com>
2023-05-19 11:48:31 -0300

    Implement routes

--- template1/src/App.tsx
+++ template1/src/App.tsx
@@ -1,16 +1,24 @@
 import { useState } from 'react'
+import {
+  Routes,
+  Route
+} from "react-router-dom";
 
 import Toolbar from './components/Toolbar/index'
 import Header from './components/Header/index'
 import Footer from './components/Footer/index'
 import Content from './components/Content/index'
 import Informations from './components/Informations/index'
+import Gallery from './components/Gallery'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
+    <Routes>
+      <Route path='galeria' element={<Gallery/>} />
+    </Routes>
       <Toolbar />
       <div className="w3-content" style={{ maxWidth: '2000px', marginTop: '46px' }}>
         <Header />
@@ -18,7 +26,7 @@ function App() {
         <Content />
       </div>
       <Footer />
-    </div>
+  </div>
   )
 }
 
