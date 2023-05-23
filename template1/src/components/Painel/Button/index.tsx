46c5d4cda731c1f659dd0a9eec775b9b7874d990
Gustavo Ferreira <gfmb94@gmail.com>
2023-05-23 20:07:33 -0300

    Update route painel

--- template1/src/components/Painel/Button/index.tsx
+++ template1/src/components/Painel/Button/index.tsx
@@ -2,7 +2,7 @@ import {Link} from "react-router-dom"
 
 function Button() {
     return (
-        <Link className="w3-button w3-round-large w3-padding w3-light-grey w3-block" style={{ fontSize: 18, textDecoration:'none' }} to={'contato'}>
+        <Link to={'/contato'} className="w3-button w3-round-large w3-padding w3-light-grey w3-block" style={{ fontSize: 18, textDecoration:'none' }}>
             <i className="fa fa-check" style={{ fontSize: 22 }}></i> Consulte agora!
         </Link>
     )
